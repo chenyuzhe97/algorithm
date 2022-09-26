18bd14e869f00e3cab32b4d971ae2fabfeb633d4
diklios <47837462+diklios5768@users.noreply.github.com>
2022-09-26 16:41:11 +0800

    增加了医学图像专用的直方图规定化（处理黑边）

--- image_processing/histogram/specification_med_img.py
+++ image_processing/histogram/specification_med_img.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
-@File Name      :   specification.py
-@Create Time    :   2022/9/23 14:39
+@File Name      :   specification_med_img.py
+@Create Time    :   2022/9/26 14:38
 @Description    :   
 @Version        :   
 @License        :   MIT
@@ -19,40 +19,27 @@ import click
 import cv2
 import numpy as np
 
+from specification import calc_probability_distribution
+
+black_threshold = 5
+
 
 def calc_hist(image):
-    # 统计元素个数
     mat = np.array(image)
-    return [len(mat[mat == i]) for i in range(256)]
+    return [len(mat[mat == i]) for i in range(black_threshold, 256)]
 
 
-def calc_probability_distribution(hist):
-    # 计算概率分布 probability distribution
-    hist = np.array(hist)
-    sum_hist = sum(hist)
-    prob_dist = hist / sum_hist
-    # 计算累计概率Cumulative probability distribution
-    cumulative_prob_dist = []
-    temp_sum = 0
-    for n in prob_dist:
-        temp_sum += n
-        cumulative_prob_dist.append(temp_sum)
-    return np.array(cumulative_prob_dist)
-
-
-# 得到图⽚像素的概率分布
 def get_infer_map(image):
     hist = calc_hist(image)
     infer_map = calc_probability_distribution(hist)
     return infer_map
 
 
-# 根据两张图的概率分布计算像素值映射关系
 def get_final_map(reflect_image_map, source_image_map):
     # 原图像素点映射到目标图的map
-    final_map = []
+    final_map = [0 for i in range(black_threshold)]
     index = 0
-    upper_limit = 255
+    upper_limit = 255 - black_threshold
     for i in range(len(source_image_map)):
         # 防⽌像素值超过255
         if index == upper_limit:
@@ -77,16 +64,10 @@ def get_new_img(source_image, reflect_image):
     source_image_map = get_infer_map(source_image)
     reflect_image_map = get_infer_map(reflect_image)
     final_map = get_final_map(reflect_image_map, source_image_map)
-
-    # 将像素点的值进⾏映射
-    # w, h = img.shape
-    # for i in range(w):
-    #     for j in range(h):
-    #         temp = img[i, j]
-    #         new_img[i, j] = final_map[temp]
-    # 快速的计算⽅法
-    for i in range(256):
+    for i in range(black_threshold, 256):
         specified_img[source_image == i] = final_map[i]
+    specified_img[source_image <= black_threshold] = 0
+
     return specified_img
 
 
@@ -121,6 +102,6 @@ def main(source_image_path, reflect_image_path, image_type):
 
 if __name__ == '__main__':
     """
-    直方图规定化
+    医学图像专用的直方图规定化
     """
     main()
